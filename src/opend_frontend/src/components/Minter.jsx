f82160dd18eafe248d4d9a23adef864e79d23cac
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2025-01-28 00:15:08 +0000

    1

--- src/opend_frontend/src/components/Minter.jsx
+++ src/opend_frontend/src/components/Minter.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+// import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 function Minter() {
+  const { register, handleSubmit} = useForm();
+
+
+  async function onSubmit(data) {
+    console.log("hello");
+    const name = data.name;
+    const image = data.image[0];
+  };
   return (
     <div className="minter-container">
       <h3 className="makeStyles-title-99 Typography-h3 form-Typography-gutterBottom">
@@ -12,6 +22,7 @@ function Minter() {
       <form className="makeStyles-form-109" noValidate="" autoComplete="off">
         <div className="upload-container">
           <input
+          {...register("image", {required:true})}
             className="upload"
             type="file"
             accept="image/x-png,image/jpeg,image/gif,image/svg+xml,image/webp"
@@ -23,6 +34,7 @@ function Minter() {
         <div className="form-FormControl-root form-TextField-root form-FormControl-marginNormal form-FormControl-fullWidth">
           <div className="form-InputBase-root form-OutlinedInput-root form-InputBase-fullWidth form-InputBase-formControl">
             <input
+            {...register("name", {required:true})}
               placeholder="e.g. CryptoDunks"
               type="text"
               className="form-InputBase-input form-OutlinedInput-input"
@@ -31,7 +43,7 @@ function Minter() {
           </div>
         </div>
         <div className="form-ButtonBase-root form-Chip-root makeStyles-chipBlue-108 form-Chip-clickable">
-          <span className="form-Chip-label">Mint NFT</span>
+          <span onClick={handleSubmit(onSubmit)} className="form-Chip-label">Mint NFT</span>
         </div>
       </form>
     </div>
